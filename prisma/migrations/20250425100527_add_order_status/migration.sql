4aad34579b856022f9f221a9852c4ee1eb1940f7
ren <kikuren1st@gmail.com>
2025-05-23 18:32:06 +0900

    マイグレーションをやり直した

--- prisma/migrations/20250425100527_add_order_status/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "test" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "test_pkey" PRIMARY KEY ("id")
-);
